2ae2efd923b561a56d2774151213145a0d82c13c
Felixdiamond <diamondfelix006@gmail.com>
2024-08-04 14:12:40 +0100

    :)

--- backend/routes/translate.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { entryFunction } = require('../controllers/translationController');
-
-router.post('/', entryFunction);
-
-module.exports = router;
